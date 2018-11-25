bf13189ba28abec60649554dc9db4b8d8771c771
Edoardo Tenani <edoardo.tenani@protonmail.com>
2018-11-25 18:01:53 +0100

    document ColorAverage

--- lib/color-average.ex
+++ lib/color-average.ex
@@ -13,6 +13,7 @@ defmodule ColorAverage do
   ## Examples
   ColorAverage.run([{0,255,255},{0,255,255},{0,0,255}]) #=> {:ok, {0,208,255}}
   """
+  @spec run(any()) :: map()
   def run(pixels) do
     red = average_red_channel_from pixels
     green = average_green_channel_from pixels
@@ -21,42 +22,18 @@ defmodule ColorAverage do
     {:ok, {red,green,blue}}
   end
 
-  @doc """
-  Calculate average color on red channel of a set of pixels
-
-  ## Examples
-  average_red_channel_from([{0,255,255},{0,255,255},{0,0,255}]) #=> 0
-  """
   defp average_red_channel_from(pixels) do
     Enum.map(pixels, fn([r,_,_]) -> r end) |> average_channel
   end
 
-  @doc """
-  Calculate average color on green channel of a set of pixels
-
-  ## Examples
-  average_green_channel_from([{0,255,255},{0,255,255},{0,0,255}]) #=> 208
-  """
   defp average_green_channel_from(pixels) do
     Enum.map(pixels, fn([_,g,_]) -> g end) |> average_channel
   end
 
-  @doc """
-  Calculate average color on blue channel of a set of pixels
-
-  ## Examples
-  average_blue_channel_from([{0,255,255},{0,255,255},{0,0,255}]) #=> 255
-  """
   defp average_blue_channel_from(pixels) do
     Enum.map(pixels, fn([_,_,b]) -> b end) |> average_channel
   end
 
-  @doc """
-  Calculate average color on a set of pixels
-
-  ## Examples
-  average_channel([0,0,0]) #=> 0
-  """
   defp average_channel(colors) do
     # first square the colors
     Enum.map(colors, fn(x) -> :math.pow(x,2) end)
