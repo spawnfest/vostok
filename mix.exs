492084f24df0519b777918b248b7eeace37d793b
Edoardo Tenani <edoardo.tenani@protonmail.com>
2018-11-25 23:03:25 +0100

    prepare hex publish

--- mix.exs
+++ mix.exs
@@ -8,7 +8,16 @@ defmodule Vostok.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       escript: [main_module: Vostok],
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      homepage_url: "https://dev.welaika.com",
+      source_url: "https://github.com/spawnfest/vostok",
+      description: "Vostok converts an image to a pixelated SVG",
+      docs: [
+        main: "Vostok",
+        logo: "static/vostok_logo_white.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -23,6 +32,16 @@ defmodule Vostok.MixProject do
   defp deps do
     [
       {:mogrify, "~> 0.6.1"}
+    ]
+  end
+
+  defp package() do
+    [
+      name: "vostok",
+      organization: "welaika",
+      files: ~w(lib config static mix.exs README* LICENSE*),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/spawnfest/vostok"}
     ]
   end
 end
