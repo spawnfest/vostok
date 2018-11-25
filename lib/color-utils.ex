a9f5b17ad9dc93f92cb7cab87d6108a8f295782f
Edoardo Tenani <edoardo.tenani@protonmail.com>
2018-11-25 18:18:48 +0100

    fix Color.Utils.hex_to_rgb spec

--- lib/color-utils.ex
+++ lib/color-utils.ex
@@ -10,7 +10,7 @@ defmodule Color.Utils do
   ## Examples
   Color.Utils.hex_to_rgb("#000000")
   """
-  @spec hex_to_rgb(string()) :: list(pos_integer(), pos_integer(), pos_integer())
+  @spec hex_to_rgb(string()) :: nonempty_list(non_neg_integer())
   def hex_to_rgb(hex) do
     String.replace(hex, "#", "")
     |> String.to_charlist()
